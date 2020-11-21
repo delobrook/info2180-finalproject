0008f351e15f46efba82368587641cd71a48613e
Delano tracey <delanotracey@gmail.com>
2020-11-21 11:33:10 -0500

    created tables

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS finalproject;
+CREATE DATABASE finalproject;
+USE finalproject;
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users(
+    id INT(8) NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    password VARCHAR(50),
+    email VARCHAR(50),
+    date_joined DATETIME,
+    PRIMARY KEY(id)
+);
+DROP TABLE IF EXISTS Issues;
+CREATE TABLE Issues(
+    id INT(8) NOT NULL AUTO_INCREMENT ,
+    title VARCHAR(50),
+    description TEXT,
+    type VARCHAR(50),
+    priority VARCHAR(50),
+    status VARCHAR(50),
+    asssigned_to INT(8),
+    created_by INT(8),
+    created DATETIME,
+    updated DATETIME,
+    PRIMARY KEY(id),
+    
+
+);
