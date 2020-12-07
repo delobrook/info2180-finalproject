007babed142fe7be6d3885b915e8a4cc28f315f7
Richard Ebanks <richard.ebanks@mymona.uwi.edu>
2020-12-06 20:18:25 -0500

    new format

--- schema.sql
+++ schema.sql
@@ -2,45 +2,34 @@ DROP DATABASE IF EXISTS finalproject;
 CREATE DATABASE finalproject;
 USE finalproject;
 
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users(
-    id INT(25) NOT NULL AUTO_INCREMENT,
-    firstname VARCHAR(50),
-    lastname VARCHAR(50),
-    password VARCHAR(50),
-    email VARCHAR(50),
-    date_joined DATETIME,
-    PRIMARY KEY(id)
-);
-DROP TABLE IF EXISTS Issues;
-CREATE TABLE Issues(
-    id INT(25) NOT NULL AUTO_INCREMENT ,
-    title VARCHAR(50),
-    description TEXT,
-    type VARCHAR(50),
-    priority VARCHAR(50),
-    status VARCHAR(50),
-    asssigned_to INT(8),
-    created_by INT(8),
-    created DATETIME,
-    updated DATETIME,
-    PRIMARY KEY(id),
-);
-INSERT INTO Users(firstname,email,password,date_joined) VALUES ('admin','admin@project2.com','password123',CURRENT_TIMESTAMP);
+--
+-- Table structure for table `users`
+--
 
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `firstname` varchar(35) NOT NULL default '',
+  `lastname` varchar(32) NOT NULL default '',
+  `password` varchar(20) NOT NULL default '',
+  `email` varchar(40) NOT NULL default '',
+  `date_joined`datetime,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-/*
-QUERIES 
+DROP TABLE IF EXISTS `issues`;
+CREATE TABLE `issues` (
+  `id` int(11) NOT NULL auto_increment,
+  `title` varchar(32) NOT NULL default '',
+  `description` text(1000) NOT NULL default '',
+  `type` varchar(32) NOT NULL default '',
+  `priority` varchar(26) NOT NULL default '',
+  `status` varchar(32) NOT NULL default '',
+  `assigned_to` int(11) default NULL,
+  `created_by` int(11),
+  `created` datetime,
+  `updates` datetime,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
-input new user into users table.
-INSERT INTO Users(firstname,lastname,password,email,date_joined) VALUES(,CURRENT_TIMESTAMP);
-
-query to get all the issues
-SELECT Issues.id,title,type,status,Users.firstname,Users.lastname,created FROM Issues INNER JOIN Users ON Issues.assigned_to=Users.id;
-
-query to input/create a new issue in the Issues table
-INSERT INTO Issues(title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES(,CURRENT_TIMESTAMP);
-
-query to get the detals of an issue
-SELECT * FROM Issues WHERE id= ;
-*/
+INSERT INTO `users`(firstname, lastname, `password`,email,date_joined) VALUES ('admin','admin', 'password123', 'admin@project2.com', NOW());
